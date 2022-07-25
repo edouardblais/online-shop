6e58a478c22d03fe5d167022073f2d05ba951a40
edouard <edouard.blais@hotmail.com>
2022-07-24 20:43:34 -0400

    setting up basic components system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Shop from './components/Shop'
+import Cart from './components/Cart'
+import Nav from './components/Nav'
+
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <BrowserRouter>
+            <Nav />
+            <Routes>
+                <Route path='/Home' element={<Home />} />
+                <Route path='/Shop' element={<Shop />} />
+                <Route path='/Cart' element={<Cart />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
