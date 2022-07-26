6615047e9870ac98de8f05b1cebb3378a9cedd76
edouard <edouard.blais@hotmail.com>
2022-07-25 22:46:14 -0400

    implementend map and display of products for  shop component

---
+++ src/components/Data.js
@@ -0,0 +1,45 @@
+import moonimage from '../images/moon.jpg';
+import callistoimage from '../images/Callisto.jpg';
+import ioimage from '../images/io.jpg';
+import charonimage from '../images/charon.jpg';
+import enceladusimage from '../images/enceladus.jpg';
+import europaimage from '../images/europa.jpg';
+import ganymedeimage from '../images/ganymede.jpg';
+import phobosimage from '../images/phobos.jpg';
+import titanimage from '../images/titan.jpg';
+import tritonimage from '../images/triton.jpg';
+
+const Data = [
+    {name:'The Moon',
+     image: moonimage,
+    },
+    {name:'Callisto',
+     image: callistoimage,
+    },
+    {name:'Io',
+     image: ioimage,
+    },
+    {name:'Charon',
+     image: charonimage,
+    },
+    {name:'Enceladus',
+     image: enceladusimage,
+    },
+    {name:'Ganymede',
+     image: ganymedeimage,
+    },
+    {name:'Europa',
+     image: europaimage,
+    },
+    {name:'Phobos',
+     image: phobosimage,
+    },
+    {name:'Titan',
+     image: titanimage,
+    },
+    {name:'Triton',
+     image: tritonimage,
+    },
+];
+
+export default Data
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Data from './Data';
 
 const Shop = () => {
     return (
         <div className="bg-gray-900 text-white flex flex-column justify-center flex-grow h-screen">
-            Shop
+            {Data?.map((mooninfo) => {
+                return ( <div key={mooninfo.name}>
+                            <div>{mooninfo.name}</div>
+                            <img alt='' src={mooninfo.image}></img>
+                         </div> 
+                )
+            })}
         </div>
     )
 }
