eec68e0ad870cc89948115926b373d65b0094e9d
edouard <edouard.blais@hotmail.com>
2022-07-28 16:55:10 -0400

    moved functions relating to cart to App.js and refactored the state management to link the cart and the totalcount, and adjusted the add and remove item functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import Shop from './components/Shop'
@@ -8,11 +8,34 @@ import Moon from './components/Moon'
 
 const App = () => {
 
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+
+    const [totalcount, setTotalcount] = useState(0);
 
     const addToCart = (mooninfo) => {
         setCart(prevState => [...prevState, mooninfo]);
         console.log(cart)
+    }
+
+    const clearCart = () => {
+        setCart([]);
+    }
+
+    useEffect(() => {
+        setTotalcount(cart.length);
+    }, [cart])
+
+    const increaseQuantity = (item) => {
+        setCart(prevState => [...prevState, item])
+    }
+
+    const decreaseQuantity = (index) => {
+        if (totalcount === 0) {
+            return
+        } else {
+            const newcart = [...cart.slice(0, index), ...cart.slice(index +1)];
+            setCart(newcart);
+        }
     }
 
   return (
@@ -22,7 +45,7 @@ const App = () => {
                 <Route path='/' element={<Home />} />
                     <Route path='/Shop' element={<Shop addToCart={addToCart}/>} />
                         <Route path='/Shop/:name' element={<Moon />} />
-                    <Route path='/Cart' element={<Cart cart={cart} />} />
+                    <Route path='/Cart' element={<Cart cart={cart} clearCart={clearCart} totalcount={totalcount} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity}/>} />
             </Routes>
         </BrowserRouter>
     )
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,31 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Cart = ({cart}) => {
-
-    const [count, setCount] = useState(cart.length);
-
-    const incrementCount = () => {
-        setCount(count + 1);
-    }
-
-    const decrementCount = () => {
-        if (count === 0) {
-            return
-        } else {
-            setCount(count - 1);
-        }
-    }
+const Cart = ({cart,
+               clearCart,
+               totalcount, 
+               increaseQuantity,
+               decreaseQuantity,
+              }) => {
 
     return (
         <div className="bg-black text-white flex flex-col items-center flex-grow h-screen">
-            <div>{count}</div>
+            <div>{totalcount}</div>
+            <button onClick={clearCart}>Clear Cart</button>
             <div>
                 {cart?.map((item, index) => (
                     
                     <div key={index}>
                         <div>{item.name}</div>
-                        <button onClick={incrementCount}>Add</button>
-                        <button onClick={decrementCount}>Remove</button>
+                        <button onClick={() => increaseQuantity(item)}>+</button>
+                        <button onClick={() => decreaseQuantity(index)}>-</button>
                     </div>
                 ))}
             </div>
