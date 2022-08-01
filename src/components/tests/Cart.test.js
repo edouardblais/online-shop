6144e136da6944eae98d97c99f70ad373fa4225d
edouard <edouard.blais@hotmail.com>
2022-07-31 21:20:25 -0400

    added test for Cart component to check that it only renders element present in cart variable

--- src/components/tests/Cart.test.js
+++ src/components/tests/Cart.test.js
@@ -1,3 +1,4 @@
+import { render, screen} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import TestRenderer from 'react-test-renderer'
 import Cart from '../Cart';
@@ -12,5 +13,33 @@ describe('Cart component', () => {
             )
             .toJSON();
         expect(cart).toMatchSnapshot();
+    })
+
+    it('renders cart elements', () => {
+        const cart = [
+            {name:'The Moon',
+             image: 'moonimage',
+             price: 90000000,
+             count: 0,
+            },
+            {name:'Callisto',
+             image: 'callistoimage',
+             price: 20000000,
+             count: 0,
+            },]
+        
+        render(<Cart cart={cart}/>, {wrapper: MemoryRouter});
+
+        const moon = screen.queryByText('The Moon');
+        expect(moon).toBeInTheDocument();
+        
+        const callisto = screen.queryByText('Callisto');
+        expect(callisto).toBeInTheDocument();
+
+        const io = screen.queryByText('Io');
+        expect(io).not.toBeInTheDocument();
+
+        const phobos = screen.queryByText('Phobos');
+        expect(phobos).not.toBeInTheDocument();
     })
 })
