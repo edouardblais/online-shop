4058628ab2c1886bb22739624caee9c893eec02e
edouard <edouard.blais@hotmail.com>
2022-08-07 12:16:55 -0400

    added content and styled Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-import '../index.css'
+import '../index.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className="bg-black text-white flex flex-col items-center flex-grow h-screen p-4 font-bold text-xl justify-center font-mono">
-            Home
+        <div className="bg-homeimage bg-cover text-white flex flex-col items-center flex-grow h-screen pt-12 justify-start font-mono gap-6 box-border">
+            <div className="text-5xl">The sky isn't the limit anymore</div>
+            <div className="text-3xl">Acquire land on any of 10 available moons</div>
+            <Link to='Shop' className="text-4xl border-white border-2 rounded p-2">Explore now!</Link>
         </div>
     )
 }
